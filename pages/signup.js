445ba0f0c92ada1ad98741fddc39fffc49dd00da
arjirayb <omgthatsdreaa@gmail.com>
2025-05-29 19:58:20 -0400

    Create signup.js

---
+++ pages/signup.js
@@ -0,0 +1,8 @@
+export default function Signup() {
+  return (
+    <div style={{ textAlign: "center", padding: "4rem", fontFamily: "sans-serif" }}>
+      <h1>Create Your Free Account</h1>
+      <p>Welcome! Your signup form will go here soon.</p>
+    </div>
+  );
+}
