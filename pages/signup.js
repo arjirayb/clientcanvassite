4dac150484759af70159b86f3ae5ce75c1159f5f
arjirayb <omgthatsdreaa@gmail.com>
2025-05-29 20:33:12 -0400

    Update signup.js

--- pages/signup.js
+++ pages/signup.js
@@ -1,11 +1,17 @@
-import styles from './Signup.module.css';
+import styles from './Signup.module.css'; // Make sure the path is correct
 
 export default function Signup() {
   return (
     <form className={styles.form}>
-      <input className={styles.input} type="text" placeholder="Name" />
-      <input className={styles.input} type="email" placeholder="Email" />
-      <input className={styles.input} type="password" placeholder="Password" />
+      <label htmlFor="name">Name</label>
+      <input className={styles.input} id="name" type="text" placeholder="Your full name" />
+
+      <label htmlFor="email">Email</label>
+      <input className={styles.input} id="email" type="email" placeholder="Your email" />
+
+      <label htmlFor="password">Password</label>
+      <input className={styles.input} id="password" type="password" placeholder="Choose a password" />
+
       <button className={styles.signupBtn}>Sign Up</button>
     </form>
   );
